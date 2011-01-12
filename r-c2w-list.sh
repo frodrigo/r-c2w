6454e776e7cc3e6e8f1279e5bda680c97984b499
Fred <fred@canari.(none)>
2011-01-12 23:42:05 +0100

    fix

--- r-c2w-list.sh
+++ r-c2w-list.sh
@@ -6,13 +6,13 @@ cat list | while read DEP REF NOM; do
     #040 HA001 "AIRE-SUR-L ADOUR"
     NOM=${NOM//\"/}
     SOURCE="${REF}-${NOM}"
-    TARGET="${DEP}-${REF}-${NOM/ /_}"
+    TARGET="${DEP}-${REF}-${NOM// /_}"
     echo $TARGET
     if [[ ! -e "${TARGET}-lands.osm" ]]; then
       $QADASTRE --download ${DEP} ${REF} "${NOM}"
       mv "${SOURCE}.pdf" "${TARGET}.pdf"
       mv "${SOURCE}.bbox" "${TARGET}.bbox"
-      $QADASTRE --convert-with-lands "${DEP}-${REF}" "${NOM/ /_}"
+      $QADASTRE --convert-with-lands "${DEP}-${REF}" "${NOM// /_}"
     fi
-#    ./r-c2w.sh "$BASE"
+    ./r-c2w.sh "${TARGET}"
 done
