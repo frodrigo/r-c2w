de2ea5d4d38c3f9e2356bc5d7d1646cf2a71e413
Fred <fred@canari.(none)>
2011-01-18 23:28:37 +0100

    no override a downloaded pdf

--- r-c2w-list.sh
+++ r-c2w-list.sh
@@ -8,10 +8,12 @@ cat list | while read DEP REF NOM; do
     SOURCE="${REF}-${NOM}"
     TARGET="${DEP}-${REF}-${NOM// /_}"
     echo $TARGET
-    if [[ ! -e "${TARGET}-lands.osm" ]]; then
+    if [[ ! -e "${TARGET}.pdf" ]]; then
       $QADASTRE --download ${DEP} ${REF} "${NOM}"
       mv "${SOURCE}.pdf" "${TARGET}.pdf"
       mv "${SOURCE}.bbox" "${TARGET}.bbox"
+    fi
+    if [[ ! -e "${TARGET}-lands.osm" ]]; then
       $QADASTRE --convert-with-lands "${DEP}-${REF}" "${NOM// /_}"
     fi
     ./r-c2w.sh "${TARGET}"
